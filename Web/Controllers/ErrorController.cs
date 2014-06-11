e3db4b43e9a225b063cd843af3803519510a2ca2
Ryan <lee.ryanjohn@gmail.com>
2014-06-11 23:38:53 +0200

    error logging

---
+++ Web/Controllers/ErrorController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Management;
+using System.Web.Mvc;
+
+
+namespace Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Index()
+        {
+            new LogEvent("message to myself").Raise();
+            return View();
+        }
+    }
+
+    public class LogEvent : WebRequestErrorEvent
+    {
+        public LogEvent(string message)
+            : base(null, null, 100001, new Exception(message))
+        {
+        }
+    }
+
+}
