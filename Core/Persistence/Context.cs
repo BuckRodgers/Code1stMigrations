a93ec9d3e78ac5ea408c72062eac0b6b63ea11fc
Ryan <lee.ryanjohn@gmail.com>
2014-06-11 23:10:06 +0200

    Context needs updating

--- Core/Persistence/Context.cs
+++ Core/Persistence/Context.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using Core.Models;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
 
 namespace Core.Persistence
 {
@@ -11,5 +13,35 @@ namespace Core.Persistence
     public class Context : DbContext
     {
         public DbSet<User> Users { get; set; }
+
+       // protected override void OnModelCreating(DbModelBuilder modelBuilder)
+       // {
+        //    modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+        //    ConfigureModel(modelBuilder);
+
+       //     Database.SetInitializer(new MigrateDatabaseToLatestVersion<Context, Configuration>());
+
+        //    base.OnModelCreating(modelBuilder);
+       // }
+
+        /*
+        private void ConfigureModel(DbModelBuilder modelBuilder)
+        {
+            var entityMethod = typeof(DbModelBuilder).GetMethod("Entity");
+
+            var entityTypes = Assembly.GetAssembly(typeof(Entity)).GetTypes()
+            .Where(x => x.IsSubclassOf(typeof(Entity)) && !x.IsAbstract);
+            foreach (var type in entityTypes)
+            {
+                entityMethod.MakeGenericMethod(type).Invoke(modelBuilder, new object[] { });
+            }
+        }
+         * */
+
+
     }
+
+   
+
 }
